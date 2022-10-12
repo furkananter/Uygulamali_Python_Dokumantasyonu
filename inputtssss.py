3c29cc0a5c867cd296d1b3b331893de518602e43
Merovingian <52575351+furkananter@users.noreply.github.com>
2022-10-12 12:10:01 +0300

    vizeNotu inputlu versiyonu

---
+++ inputtssss.py
@@ -0,0 +1,5 @@
+vizeNotu = float(input("Vize notunuzu giriniz: "))
+finalNotu = float(input("Final notunuzu giriniz: "))
+gecmeNotu = float((vizeNotu*0.4) + (finalNotu*0.6))
+if gecmeNotu >= 50:
+        print("aferin geçtin")
